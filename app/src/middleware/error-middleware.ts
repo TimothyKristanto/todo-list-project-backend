a96b093e12cf8615c0988202133e323a2959f000
TimothyKristanto <66807730+TimothyKristanto@users.noreply.github.com>
2024-12-09 17:07:06 +0700

    Improved Error Message for Zod Error
    1. Clarify the error message for Zod Error type
    
    2. Removed variable to save db delete result in deleteTodo service function

--- app/src/middleware/error-middleware.ts
+++ app/src/middleware/error-middleware.ts
@@ -5,22 +5,22 @@ import { ResponseError } from "../error/response-error"
 // Every error respond from this API will go through this error middleware
 // This middleware will check whether there is an error or not, if an error exists, then return error message
 export const errorMiddleware = async (
-	error: Error,
-	req: Request,
-	res: Response,
-	next: NextFunction
+    error: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-	if (error instanceof ZodError) {
-		res.status(400).json({
-			errors: `Validation error: ${JSON.stringify(error)}`,
-		})
-	} else if (error instanceof ResponseError) {
-		res.status(error.status).json({
-			errors: error.message,
-		})
-	} else {
-		res.status(500).json({
-			errors: error.message,
-		})
-	}
+    if (error instanceof ZodError) {
+        res.status(400).json({
+            errors: `Validation error: ${JSON.stringify(error.message)}`,
+        })
+    } else if (error instanceof ResponseError) {
+        res.status(error.status).json({
+            errors: error.message,
+        })
+    } else {
+        res.status(500).json({
+            errors: error.message,
+        })
+    }
 }
--- app/src/service/todo-service.ts
+++ app/src/service/todo-service.ts
@@ -1,10 +1,10 @@
 import { Todo, User } from "@prisma/client"
 import {
-	TodoCreateRequest,
-	TodoResponse,
-	TodoUpdateRequest,
-	toTodoResponse,
-	toTodoResponseList,
+    TodoCreateRequest,
+    TodoResponse,
+    TodoUpdateRequest,
+    toTodoResponse,
+    toTodoResponseList,
 } from "../model/todo-model"
 import { prismaClient } from "../application/database"
 import { Validation } from "../validation/validation"
@@ -13,92 +13,92 @@ import { ResponseError } from "../error/response-error"
 import { logger } from "../application/logging"
 
 export class TodoService {
-	static async getAllTodo(user: User): Promise<TodoResponse[]> {
-		const todo = await prismaClient.todo.findMany({
-			where: {
-				user_id: user.id,
-			},
-		})
+    static async getAllTodo(user: User): Promise<TodoResponse[]> {
+        const todo = await prismaClient.todo.findMany({
+            where: {
+                user_id: user.id,
+            },
+        })
 
-		return toTodoResponseList(todo)
-	}
+        return toTodoResponseList(todo)
+    }
 
-	static async getTodo(user: User, todo_id: number): Promise<TodoResponse> {
-		const todo = await this.checkTodoIsEmpty(user.id, todo_id)
+    static async getTodo(user: User, todo_id: number): Promise<TodoResponse> {
+        const todo = await this.checkTodoIsEmpty(user.id, todo_id)
 
-		return toTodoResponse(todo)
-	}
+        return toTodoResponse(todo)
+    }
 
-	static async createTodo(
-		user: User,
-		req: TodoCreateRequest
-	): Promise<string> {
-		// validate request
-		const todoRequest = Validation.validate(TodoValidation.CREATE, req)
+    static async createTodo(
+        user: User,
+        req: TodoCreateRequest
+    ): Promise<string> {
+        // validate request
+        const todoRequest = Validation.validate(TodoValidation.CREATE, req)
 
-		const todo = await prismaClient.todo.create({
-			data: {
-				title: todoRequest.title,
-				description: todoRequest.description,
-				status: todoRequest.status,
-				priority: todoRequest.priority,
-				due_date: todoRequest.due_date,
-				user_id: user.id,
-			},
-		})
+        const todo = await prismaClient.todo.create({
+            data: {
+                title: todoRequest.title,
+                description: todoRequest.description,
+                status: todoRequest.status,
+                priority: todoRequest.priority,
+                due_date: todoRequest.due_date,
+                user_id: user.id,
+            },
+        })
 
-		return "Data created successfully!"
-	}
+        return "Data created successfully!"
+    }
 
-	static async checkTodoIsEmpty(
-		user_id: number,
-		todo_id: number
-	): Promise<Todo> {
-		const todo = await prismaClient.todo.findUnique({
-			where: {
-				id: todo_id,
-				user_id: user_id,
-			},
-		})
+    static async checkTodoIsEmpty(
+        user_id: number,
+        todo_id: number
+    ): Promise<Todo> {
+        const todo = await prismaClient.todo.findUnique({
+            where: {
+                id: todo_id,
+                user_id: user_id,
+            },
+        })
 
-		if (!todo) {
-			throw new ResponseError(400, "Todo not found!")
-		}
+        if (!todo) {
+            throw new ResponseError(400, "Todo not found!")
+        }
 
-		return todo
-	}
+        return todo
+    }
 
-	static async updateTodo(
-		user: User,
-		req: TodoUpdateRequest
-	): Promise<string> {
-		const todoValidation = Validation.validate(TodoValidation.UPDATE, req)
+    static async updateTodo(
+        user: User,
+        req: TodoUpdateRequest
+    ): Promise<string> {
+        const todoValidation = Validation.validate(TodoValidation.UPDATE, req)
 
-		await this.checkTodoIsEmpty(user.id, todoValidation.id)
+        await this.checkTodoIsEmpty(user.id, todoValidation.id)
 
-		const todoUpdate = await prismaClient.todo.update({
-			where: {
-				id: todoValidation.id,
-				user_id: user.id,
-			},
-			data: todoValidation,
-		})
+        const todoUpdate = await prismaClient.todo.update({
+            where: {
+                id: todoValidation.id,
+                user_id: user.id,
+            },
+            data: todoValidation,
+        })
 
-		logger.info("UPDATE RESULT: " + todoUpdate)
+        logger.info("UPDATE RESULT: " + todoUpdate)
 
-		return "Data update was successful!"
-	}
+        return "Data update was successful!"
+    }
 
-	static async deleteTodo(user: User, todo_id: number): Promise<String> {
-		await this.checkTodoIsEmpty(user.id, todo_id)
+    static async deleteTodo(user: User, todo_id: number): Promise<String> {
+        await this.checkTodoIsEmpty(user.id, todo_id)
 
-		const todoDelete = await prismaClient.todo.delete({
-			where: {
-				user_id: user.id,
-				id: todo_id,
-			},
-		})
+        await prismaClient.todo.delete({
+            where: {
+                user_id: user.id,
+                id: todo_id,
+            },
+        })
 
-		return "Data has been deleted successfully!"
-	}
+        return "Data has been deleted successfully!"
+    }
 }
